201349bead7bd115375e10848faed2cbba762379
Tokiua <josuepiensa2023.20@gmail.com>
2026-03-04 09:58:44 -0600

    actualizacion de iconos

--- partials/Includes/header.php
+++ partials/Includes/header.php
@@ -13,17 +13,17 @@ $themeHex = isset($themeHex) ? $themeHex : '#7c3aed';
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;600;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="icon" type="image/png" href="../../assets/img/nexosyne.ico">
-    <!-- Favicon navegador -->
-    <link rel="icon" type="image/png" sizes="16x16" href="../../assets/img/16x16.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="../../assets/img/32x32.png">
+    <!-- Favicon -->
+    <link rel="icon" sizes="16x16" href="/assets/img/16x16.png">
+    <link rel="icon" sizes="32x32" href="/assets/img/32x32.png">
 
     <!-- iPhone -->
-    <link rel="apple-touch-icon" sizes="180x180" href="../../assets/img/180x180.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/assets/img/180x180.png">
 
-    <!-- Android PWA -->
-    <link rel="manifest" href="manifest.json">
+    <!-- PWA -->
+    <link rel="manifest" href="/manifest.json">
 
-    <meta name="theme-color" content="#000000">
+    <meta name="theme-color" content="#7c3aed">
     <meta name="mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
